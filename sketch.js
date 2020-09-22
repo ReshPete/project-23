98f4df14117a60405fcf80b8d23f8ea79894b407
anshwhitehtjr <69585432+anshwhitehtjr@users.noreply.github.com>
2020-09-22 21:14:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,71 +1,70 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground,pboxl,pboxm,pboxr;
+var paper,groundSprite,pboxl,pboxm,pboxr,pbodyl,pbodym,pbodyr;
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
-}
+var engine,world;
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 600);
 	rectMode(CENTER);
 	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
-	engine = Engine.create();
+    engine= Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
-	World.add(world, packageBody);
 	
 
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255);
+
+	paper = new Paper(400,400,30,30);
+    
+	
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
 
-	Engine.run(engine);
+	
   
-	pboxl = createSprite(250,groundSprite.y-55,20,100);
+	pboxl = createSprite(550,groundSprite.y-55,20,100);
 	pboxl.shapeColor=color("red");
-
-	pboxm = createSprite(350,groundSprite.y-10,200,10);
+ 
+	pboxm = createSprite(650,groundSprite.y-10,200,10);
 	pboxm.shapeColor=color("red");
 
-	pboxr = createSprite(450,groundSprite.y-55,20,100);
+	pboxr = createSprite(750,groundSprite.y-55,20,100);
 	pboxr.shapeColor=color("red");
+
+
+	pbodyl = Bodies.rectangle(550,groundSprite.y-55,20,100);
+	World.add(world, pbodyl);
+
+	pbodym = Bodies.rectangle(650,groundSprite.y-10,200,10);
+	World.add(world, pbodym);
+
+	pbodyr = Bodies.rectangle(750,groundSprite.y-55,20,100);
+	World.add(world, pbodyr);
+
+	Engine.run(engine);
+
 }
+
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  paper.display();
   drawSprites();
- 
+
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-	Matter.Body.setStatic(packageBody,false);
+	Matter.Body.applyForce(paper.body,paper.body.position,{x : 85,y : -85});
 	
   }
 }
